c92ab8ac94bc544227e77283d9d02768ecbfb80a
sixtos04@gmail.com <sixtos04@gmail.com>
2022-11-02 02:20:32 -0600

    Servidor intreacciona con getwiki.py ok

--- Servidor/Program.cs
+++ Servidor/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -6,10 +7,29 @@ namespace Servidor
 {
     class Program
     {
-        private const int MAX_BYTES = 1024;
+        private const int MAX_BYTES = 10240;
+
+        static void Python_wiki_script(string qry)
+        {
+            Process proc = new();
+            proc.StartInfo = new ProcessStartInfo()
+            {
+                FileName = "python",
+                Arguments = string.Format("{0} {1}", "getwiki.py", qry),
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = false
+            };
+            proc.Start();
+            string proc_out = proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
+            Console.WriteLine("Python output: " + proc_out);
+            return;
+        }
 
         static void Main(string[] args)
         {
+            System.Console.Title = "Servidor";
             // localhost 127.0.0.1
             IPHostEntry host = Dns.GetHostEntry("localhost");
             IPAddress ip_addr = host.AddressList[0];
@@ -25,11 +45,11 @@ namespace Servidor
 
                 Socket handler = listener.Accept();
 
-                string msg_recvd = null;
+                string msg_recvd, wiki_out;
                 byte[] bytes_recvd = new byte[MAX_BYTES];
                 int num_bytes_recvd;
 
-                while(true)
+                while (true)
                 {
                     while (true)
                     {
@@ -39,10 +59,17 @@ namespace Servidor
                         if (msg_recvd.IndexOf("<EOF>") > -1)
                             break;
                     }
-                    string clean_data = msg_recvd[..^5];
+                    string clean_query = msg_recvd[..^5];
 
-                    global::System.Console.WriteLine("Cliente: " + clean_data);
-                    byte[] response_bytes = Encoding.ASCII.GetBytes("Mensaje recibido.");
+                    global::System.Console.WriteLine("Cliente: " + clean_query);
+
+                    Python_wiki_script(clean_query);
+
+                    // read wiki output from wiki_out.temp into string
+                    wiki_out = System.IO.File.ReadAllText("wiki_out.tmp");
+                    byte[] response_bytes = Encoding.UTF8.GetBytes(wiki_out);
+                    
+                    // send string_out back to client
                     handler.Send(response_bytes);
                 }
             }
